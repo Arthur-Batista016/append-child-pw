4247112aa4e5b07c2634c7234f31995872bbfac1
Arthur-Batista016 <arthurbonvecchiobatista@gmail.com>
2023-09-27 20:39:12 -0300

    feat organizei o codigo

--- js/append.js
+++ js/append.js
@@ -8,11 +8,15 @@ function Cadastrar(){
     const lista_item = document.createElement("li")
     
     const prod_nome  = document.createElement("h3")
+    
     const prod_marca = document.createElement("h4") 
+    
     const prod_preco = document.createElement("p")
+    
     prod_nome.innerHTML = produto.nome
     prod_marca.innerHTML = produto.marca
     prod_preco.innerHTML = produto.preco
+    
     lista_item.appendChild(prod_nome)
     lista_item.appendChild(prod_marca)
     lista_item.appendChild(prod_preco)
